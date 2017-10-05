eb633887162df55ed5647275d39d5ecd37b3e7d9
Marcelo Izaguirre <marcelo.izaguirre@ni.com>
2017-10-05 11:24:07 +0300

    Asd simple validation test for pythonnet

--- tests/setup/test_validatesetup.py
+++ tests/setup/test_validatesetup.py
@@ -5,3 +5,11 @@ def _inc(x):
 def test_pass():
     """Just make sure asserts work."""
     assert _inc(4) == 5
+
+
+def test_pythonnetworks():
+    import clr
+    clr.AddReference("System")
+    from System import String
+    s = String("teststring")
+    assert "teststring" == s
