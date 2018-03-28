72d7fd6d79a470b81f15a2514a4351d49b1ab974
Marcelo Izaguirre <marcelo.izaguirre@ni.com>
2018-03-28 15:03:34 +0000

    Merged PR 148: Load assemblies from GAC first, install second
    Load assemblies from GAC first, install/dev environment second

--- src/niveristand/_internal.py
+++ src/niveristand/_internal.py
@@ -1,17 +1,37 @@
 import os
 import tempfile
 import clr
-import yaml
 
 
 def base_assembly_path():
     try:
         return _getdevconfig()['BaseBinariesPath']
     except (IOError, KeyError):
+        pass
+    try:
+        return _get_installed_path()
+    except IOError:
         return ''
 
 
+def _get_installed_path():
+    import winreg
+    latest_dir = ''
+
+    with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Wow6432Node\\National Instruments\\VeriStand\\') as vskey:
+        r = winreg.QueryInfoKey(vskey)
+        ver = 0
+        for k in range(r[0]):
+            with winreg.OpenKey(vskey, winreg.EnumKey(vskey, k)) as this_key:
+                this_ver = int(winreg.QueryValueEx(this_key, 'VersionString')[0])
+                if this_ver > ver:
+                    latest_dir = winreg.QueryValueEx(this_key, 'InstallDir')[0]
+                    ver = this_ver
+    return os.path.join(latest_dir, 'nivs.lib', 'Reference Assemblies')
+
+
 def _getdevconfig():
+    import yaml
     cfgfile = os.environ["vsdev.yaml"]
     cfgfile = cfgfile.strip('"')
     with open(os.path.normpath(cfgfile), "r") as f:
@@ -21,14 +41,22 @@ def _getdevconfig():
 
 clr.AddReference("System")
 clr.AddReference("System.IO")
-clr.AddReference(os.path.join(base_assembly_path(),
-                              "NationalInstruments.VeriStand.RealTimeSequenceDefinitionApi.dll"))
-clr.AddReference(os.path.join(base_assembly_path(),
-                              "NationalInstruments.VeriStand.RealTimeSequenceDefinitionApiUtilities.dll"))
-clr.AddReference(os.path.join(base_assembly_path(),
-                              "NationalInstruments.VeriStand.DataTypes.dll"))
-clr.AddReference(os.path.join(base_assembly_path(),
-                              "NationalInstruments.VeriStand.ClientAPI.dll"))
+from System.IO import FileNotFoundException  # noqa: E402, I202 .net imports can't be at top of file.
+try:
+    # Try loading from the GAC first, dev/install folders second.
+    clr.AddReference("NationalInstruments.VeriStand.RealTimeSequenceDefinitionApi")
+    clr.AddReference("NationalInstruments.VeriStand.RealTimeSequenceDefinitionApiUtilities")
+    clr.AddReference("NationalInstruments.VeriStand.DataTypes")
+    clr.AddReference("NationalInstruments.VeriStand.ClientAPI")
+except FileNotFoundException:
+    clr.AddReference(os.path.join(base_assembly_path(),
+                                  "NationalInstruments.VeriStand.RealTimeSequenceDefinitionApi.dll"))
+    clr.AddReference(os.path.join(base_assembly_path(),
+                                  "NationalInstruments.VeriStand.RealTimeSequenceDefinitionApiUtilities.dll"))
+    clr.AddReference(os.path.join(base_assembly_path(),
+                                  "NationalInstruments.VeriStand.DataTypes.dll"))
+    clr.AddReference(os.path.join(base_assembly_path(),
+                                  "NationalInstruments.VeriStand.ClientAPI.dll"))
 
 
 def dummy():
