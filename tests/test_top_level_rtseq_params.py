dc3f14716ea17b2c130da79723a7855524f76f05
Ioan Cornea <ioan.cornea@ni.com>
2019-01-18 18:21:48 +0200

    add more tests and update engine demo example

--- tests/test_top_level_rtseq_params.py
+++ tests/test_top_level_rtseq_params.py
@@ -1,7 +1,9 @@
 from niveristand import nivs_rt_sequence
 from niveristand import NivsParam
 from niveristand import realtimesequencetools
-from niveristand.clientapi import ChannelReference, DoubleValue
+from niveristand.clientapi import ChannelReference, DoubleValue, DoubleValueArray, RealTimeSequence
+from niveristand.errors import VeristandError
+import pytest
 
 
 @NivsParam('p', DoubleValue(1.2), NivsParam.BY_REF)
@@ -10,13 +12,80 @@ def func1(p):
     return p.value
 
 
-def test_func1_numeric():
+@NivsParam('p', DoubleValueArray([1.2, 2.3]), NivsParam.BY_VALUE)
+@nivs_rt_sequence
+def func2(p):
+    return p[0].value
+
+
+def test_run_py_as_rtseq_numeric_param():
     actual = realtimesequencetools.run_py_as_rtseq(func1, {"p": DoubleValue(2.3)})
     assert actual == 2.3
 
 
-def test_func1_channel_reference():
+def test_run_py_as_rtseq_channel_reference_param():
     desired_rpm = ChannelReference('Aliases/DesiredRPM')
     desired_rpm.value = 100.101
     actual = realtimesequencetools.run_py_as_rtseq(func1, {"p": desired_rpm})
     assert actual == 100.101
+
+
+def test_run_py_as_rtseq_invalid_extra_parameter():
+    with pytest.raises(VeristandError):
+        realtimesequencetools.run_py_as_rtseq(func1, {"p": DoubleValue(2.3), "pp": DoubleValue(3.4)})
+
+
+def test_run_py_as_rtseq_missing_by_ref_parameter():
+    with pytest.raises(VeristandError):
+        realtimesequencetools.run_py_as_rtseq(func1, {})
+
+
+def test_run_py_as_rtseq_missing_by_value_parameter():
+    actual = realtimesequencetools.run_py_as_rtseq(func2, {})
+    assert actual == 1.2
+
+
+def test_run_py_as_rtseq_wrong_parameter_data_type():
+    with pytest.raises(VeristandError):
+        realtimesequencetools.run_py_as_rtseq(func2, {"p": DoubleValue(2.3)})
+
+
+def test_realtimesequence_numeric_param():
+    rtseq = RealTimeSequence(func1)
+    actual = rtseq.run({"p": DoubleValue(2.3)})
+    actual.wait_for_result()
+    assert actual.ret_val == 2.3
+
+
+def test_realtimesequence_channel_reference_param():
+    desired_rpm = ChannelReference('Aliases/DesiredRPM')
+    desired_rpm.value = 100.101
+    rtseq = RealTimeSequence(func1)
+    actual = rtseq.run({"p": desired_rpm})
+    actual.wait_for_result()
+    assert actual.ret_val == 100.101
+
+
+def test_realtimesequence_invalid_extra_parameter():
+    rtseq = RealTimeSequence(func1)
+    with pytest.raises(VeristandError):
+        rtseq.run({"p": DoubleValue(2.3), "pp": DoubleValue(3.4)})
+
+
+def test_realtimesequence_missing_by_ref_parameter():
+    rtseq = RealTimeSequence(func1)
+    with pytest.raises(VeristandError):
+        rtseq.run({})
+
+
+def test_realtimesequence_missing_by_value_parameter():
+    rtseq = RealTimeSequence(func2)
+    actual = rtseq.run({})
+    actual.wait_for_result()
+    assert actual.ret_val == 1.2
+
+
+def test_realtimesequence_wrong_parameter_data_type():
+    rtseq = RealTimeSequence(func2)
+    with pytest.raises(VeristandError):
+        rtseq.run({"p": DoubleValue(2.3)})
