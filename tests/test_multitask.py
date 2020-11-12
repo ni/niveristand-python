ecc65c75f275619e657fb79ecf4c8c605ceb29eb
Ioana Popa <ioana.popa@ni.com>
2020-11-12 18:24:01 +0200

    Suppress flake8 warning

--- tests/test_multitask.py
+++ tests/test_multitask.py
@@ -291,7 +291,7 @@ def multitask_duplicate_name_fails():
         def f1():
             a.value = 1
 
-        @task(mt)
+        @task(mt)  # noqa: F811 redefinition is exactly what we're testing here.
         def f1():  # noqa: F811 redefinition is exactly what we're testing here.
             a.value = 2
     return a.value
