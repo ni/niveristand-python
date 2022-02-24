94aa5da510b6f8bd65045b99c4f3f864c0374889
r4jmond <domino5740@o2.pl>
2022-02-24 19:13:37 +0100

    Small updates and cleaning the legacy_basic_reconnect example

--- examples/legacy_basic_reconnect.py
+++ examples/legacy_basic_reconnect.py
@@ -3,21 +3,15 @@ import time
 from niveristand.errors import RunError
 from niveristand.legacy import NIVeriStand
 
-# This is an example script to demonstrate the usage of NIVeriStand Python API.
-# This script requires Python 32b(tested on Python 3.9.6) and NIVeriStand Python API.
-# It can be downloaded here: https://niveristand-python.readthedocs.io/en/latest/index.html
+# This is an example script to demonstrate the usage of ReconnectToSystem function.
+# This script requires cPython(tested on cPython 3.9.6) and (obviously) NIVeriStand Python API.
 
 # This script deploys example project on the selected GATEWAY and it prints two channel values every 100ms
-# This script requires VeriStand serwer to be running. It is ran automatically after launching VeriStand.
-# You can also start it manually from the command line.
-# Default veristand-server.exe location:
-# "C:\Program Files (x86)\National Instruments\VeriStand 2020\veristand-server.exe"
+# This script requires the example SineWave Delay Project(shipped with LabVIEW 2020) to be deployed.
+# SineWave Delay project file path: C:\Users\Public\Documents\National Instruments\NI VeriStand 2020\Projects\Example\Sinewave Delay.nivsproj
 # Press Ctrl + C to stop the program.
 
-PROJECTDIR = r"C:\Users\Public\Documents\National Instruments\NI VeriStand 2020\Projects"
-SDF = PROJECTDIR + r"\Example\Sinewave Delay.nivssdf"
-
-GATEWAY = "localhost"  # GATEWAY address
+GATEWAY = "localhost"
 TARGET = "Controller"
 
 SINE_WAVE = 'Aliases/SineWave'
@@ -31,8 +25,6 @@ def sleep():
 
 wks = NIVeriStand.Workspace2(GATEWAY)  # create workspace object
 try:
-    # print("Running %s" % SDF)
-    # wks.ConnectToSystem(SDF,True,60000)  # connect to system, deploy project, 60s timeout
     print("Connecting to the gateway %(gateway)s, target %(target)s" % {'gateway': GATEWAY, "target": TARGET})
     wks.ReconnectToSystem(TARGET, True, None, 60000)  # reconnect to already deployed system
     print("NI VeriStand Running and Client Connected!")
@@ -55,4 +47,3 @@ except Exception:
     time.sleep(10)
 finally:
     wks.DisconnectFromSystem('', True)  # undeploy project
-    # pass
