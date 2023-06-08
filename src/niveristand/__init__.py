6972729fef2d3b456261326054488b792e1fc369
Aaron Cederquist <41800723+aaron-ni@users.noreply.github.com>
2023-06-08 15:31:53 -0500

    adding _auto_generated_classes (SDF API) content into niveristand submodule's __init__.py

--- src/niveristand/__init__.py
+++ src/niveristand/__init__.py
@@ -4,10 +4,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from niveristand._decorators import nivs_rt_sequence, NivsParam
 from niveristand.realtimesequencetools import run_py_as_rtseq, save_py_as_rtseq
+from ._auto_generated_classes import VeriStandSdfError, ErrorCode, XMLVersionInfo
 
 __all__ = [
     "NivsParam",
     "nivs_rt_sequence",
     "run_py_as_rtseq",
     "save_py_as_rtseq",
+    "VeriStandSdfError",
+    "ErrorCode",
+    "XMLVersionInfo",
 ]
