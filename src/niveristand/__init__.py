f43f95c8cdc8e4ab523acd672f62a383b0bf321b
Aaron Cederquist <41800723+aaron-ni@users.noreply.github.com>
2023-06-08 16:25:32 -0500

    fixed flake8 error

--- src/niveristand/__init__.py
+++ src/niveristand/__init__.py
@@ -4,14 +4,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from niveristand._decorators import nivs_rt_sequence, NivsParam
 from niveristand.realtimesequencetools import run_py_as_rtseq, save_py_as_rtseq
-from ._auto_generated_classes import VeriStandSdfError, ErrorCode, XMLVersionInfo
+from ._auto_generated_classes import ErrorCode, VeriStandSdfError, XMLVersionInfo
 
 __all__ = [
+    "ErrorCode",
     "NivsParam",
     "nivs_rt_sequence",
     "run_py_as_rtseq",
     "save_py_as_rtseq",
     "VeriStandSdfError",
-    "ErrorCode",
     "XMLVersionInfo",
 ]
