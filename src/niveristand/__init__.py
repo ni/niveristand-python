e9dd0335adcbff61bf66ece015fd4a8c5c3d4b8b
Paul Power <paul.power@ni.com>
2024-04-11 16:32:43 -0500

    undo init change

--- src/niveristand/__init__.py
+++ src/niveristand/__init__.py
@@ -1,4 +1,14 @@
-"""Module for NationalInstruments.VeriStand."""
-### AUTO-GENERATED CODE - DO NOT MODIFY DIRECTLY ###
+from niveristand import _internal  # noqa: F401: loads the .NET dlls for subsequent imports
+from niveristand._auto_generated_classes import ErrorCode, VeriStandSdfError, XMLVersionInfo
+from niveristand._decorators import nivs_rt_sequence, NivsParam
+from niveristand.realtimesequencetools import run_py_as_rtseq, save_py_as_rtseq
 
-from ._auto_generated_classes import *
+__all__ = [
+    "ErrorCode",
+    "NivsParam",
+    "nivs_rt_sequence",
+    "run_py_as_rtseq",
+    "save_py_as_rtseq",
+    "VeriStandSdfError",
+    "XMLVersionInfo",
+]
