651e0e84da8ba5e657c770fc02470c5383dbb524
Ashutosh Kumar <ashutosh.kumar@ni.com>
2024-08-27 20:50:36 +0530

    Update versions in some files

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = str(datetime.datetime.now().year) + ", National Instruments"
 author = "National Instruments"
 
 # The short X.Y version
-version = "3.1"
+version = "3.2"
 # The full version, including alpha/beta/rc tags
-release = "3.1.0"
+release = "3.2.0"
 
 # -- General configuration ---------------------------------------------------
 
